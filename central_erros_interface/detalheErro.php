7329ce1d61dfddfd9d5f73ec86623661b99710d2
edgarsovinski <edgarsovinski@yahoo.com.br>
2019-11-22 13:27:33 -0300

    adicionado arquivos para tratamento de detalheErro.php e movidos arquivos .js para a pasta script

--- central_erros_interface/detalheErro.php
+++ central_erros_interface/detalheErro.php
@@ -1,12 +1,12 @@
 <!doctype html>
-<html lang="en">
-<?php
+<html lang="pt-br">
+<?php 
+	
 	session_start();	
 ?>
-
 <head>
     <!-- Required meta tags -->
-    <title>Cadastro</title>
+    <title>Detalhe Log</title>
     
     <meta charset="utf-8">
     <meta name="description" content="Projeto Final Squad 1.">
@@ -19,16 +19,21 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
 
     <style type="text/css">
-		  .msg-erro{ color: red; background-color: white; }
+	  .msg-erro{ color: red; background-color: white; }
       .msg-alerta{ background-color: yellow; }
       .msg-exito{ color: green; background-color: white; }
       .msg-processando{ color: DodgerBlue; background-color: white; }
     </style>
   </head>
   <body>
-    <div class="login-clean">
+    <div class="container-fluid">
+
+      <span class="text-muted">bem vindo sr. <?php echo $_SESSION['email']; ?>, O Chupa-Cabras proprietário do token: <?php echo $_SESSION['token']; ?>.</span>
 
       <!-- falhas em geral como as lanÃ§adas pelo serviÃ§o -->
       <span class='msg-erro msg-falha'></span>
@@ -37,33 +42,29 @@
       <!-- exemplos: cadastrado, alterado, excluido com sucesso -->
       <span class='msg-exito msg-sucesso'></span>
 
-      <h1 class="text-center">Cadastro usuario</h1>
-      <form action="" method="post" id="forme" name="form">
-          <div class="form-group">
-              <input type="email" name="email" id="email" class="form-control" placeholder="e-mail"><br>
-              <input type="password" name="senha" id="senha" class="form-control" placeholder="password">
-          </div>
+      
+      <input type="button" id="cons" class="btn btn-primary btn-block" value="Voltar">
 
-        <input type="submit"  class="btn btn-primary btn-block" value="Enviar">
-        <input type="button" id="cons" class="btn btn-primary btn-block" value="Consultar">
-      </form>
+      <h1 class="text-center">Erro no "IP" em "DATA"</h1>
 
+      <h2 class="text-center">Título</h1>
+      <h3 class="text-center">"TITULO"</h1>
       <br>
       <br>
-      <!-- 
-      <input type="button" id="cons" class="btn btn-primary btn-block" value="Consultar">
-      <input type="button" id="cons" value="Consultar">
-      -->
+      <h2 class="text-center">Detalhes</h1>
+      <h3 class="text-center">"DETALHES"</h1>
+      <br>
+      <br>
 
     </div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>  
     <script src="./script/requisicao.js"></script>
-    <script src="./script/requisicaoAjax.js"></script>    
+    <script src="./script/requisicaoAjax.js"></script>
     <script src="./script/comum.js"></script>
-    <script src="./script/index.js"></script>
+    <script src="./script/detalheErro.js"></script>
   </body>
 </html>
 
--- central_erros_interface/tabelaErros.php
+++ central_erros_interface/tabelaErros.php
@@ -6,7 +6,7 @@
 ?>
 <head>
     <!-- Required meta tags -->
-    <title>Home</title>
+    <title>Logues</title>
     
     <meta charset="utf-8">
     <meta name="description" content="Projeto Final Squad 1.">
@@ -31,7 +31,7 @@
     </style>
   </head>
   <body>
-    <div class="login-clean">
+    <div class="container-fluid">
 
       <span class="text-muted">bem vindo sr. <?php echo $_SESSION['email']; ?>, O Chupa-Cabras proprietário do token: <?php echo $_SESSION['token']; ?>.</span>
 
@@ -71,15 +71,22 @@
       <input type="button" id="cons" class="btn btn-primary btn-block" value="Arquivar">
       <input type="button" id="cons" class="btn btn-primary btn-block" value="Apagar">
 
+      <table 
+        id="tabelaResultado"
+        data-classes="table table-striped table-condensed" 
+        data-show-columns="true"
+        data-show-refresh="true" >			
+      </table>
+
     </div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>  
-    <script src="./requisicao.js"></script>
-    <script src="./requisicaoAjax.js"></script>
-    <script src="./comum.js"></script>
-    <script src="./tabelaErros.js"></script>
+    <script src="./script/requisicao.js"></script>
+    <script src="./script/requisicaoAjax.js"></script>
+    <script src="./script/comum.js"></script>
+    <script src="./script/tabelaErros.js"></script>
   </body>
 </html>
 
