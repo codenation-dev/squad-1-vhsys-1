56bb59be45929613f9e42da95fae8081507ff133
Melchiades de Medeiros Neto <55635827+melchiadesx@users.noreply.github.com>
2019-12-07 16:25:39 -0300

    Adicionado not found, correções de bugs e inserção de rotas/token/ambiente.
    Adicionado not found, correções de bugs e inserção de rotas/token/ambiente.

--- filtrarErros.php
+++ filtrarErros.php
@@ -1,15 +1,17 @@
 <?php
-ini_set('max_execution_time', 300);
+ini_set('max_execution_time', 0);
 $newHashedFile = null;
 $numeroAntigo = -1;
 $url = "localhost/central/erro";//url que vai fazer a requisição do cURL
-//Mudar a configuração pra conseguir pegar o erro escrever no httpd. conf do apache
-//ErrorLogFormat "%tV%lVpid %PV%F: %E: Vclient %aV%M"
+//Mudar a configuração pra conseguir pegar o erro
+$ambiente = readline("Digite o seu ambiente. Dev/Homologação: ");
+$token = readline("\rDigite seu token: ");
+$caminho = readline("\rInforme o caminho do log: ");
 while(true){
     sleep(5);
-    $hashedFile = md5_file('C:\xampp\apache\logs\error.log');
+    $hashedFile = md5_file($caminho);
     if($hashedFile !== $newHashedFile){
-        $arquivo = fopen('C:\xampp\apache\logs\error.log','r');
+        $arquivo = fopen($caminho,'r');
         $numeroAtual = 0;
         while($linha = fgets($arquivo)){
             //if para fazer o while n ter processos enquanto nao tiver novo item no log
@@ -21,29 +23,49 @@ while(true){
             //Lembrar de colocar o código no documento do apache.
             $pos2 = strpos($linha, 'VcritV');
             $pos3 = strpos($linha,'VerrorV');
-            $pos4 = strpos($linha,'VwarnV');
-            $pos5 = strpos($linha,'VnoticeV');
             if($pos2 !==false||$pos3!==false ){
-                
+                $errorNotFound = strpos($linha, 'not found or unable to stat');
+                if($errorNotFound!==false){
+                    
                 $coisasNaLinha = explode('V',$linha);//todos os itens da linha em array
-                $ultimaCoisaNaLinha = explode(':',$coisasNaLinha[5]);//ultimo item da linha, com o titulo e descrição
-                $titulo = $ultimaCoisaNaLinha[0];
+                $ultimaCoisaNaLinha = explode("'",$coisasNaLinha[5]);
+                $titulo = $ultimaCoisaNaLinha[0] ." ". $ultimaCoisaNaLinha[2];
                 $level = $coisasNaLinha[1];
                 $dataHora = $coisasNaLinha[0];
-                if($ultimaCoisaNaLinha[3]==null){
-                    $origem = str_replace('\\\\','\\',explode('in C', $ultimaCoisaNaLinha[2]));
-                }else{
-                    $origem = str_replace('\\\\','\\',explode('in C', $ultimaCoisaNaLinha[3]));
-                }
+                $origem = $ultimaCoisaNaLinha[1];
                 $ip = explode('client ',$coisasNaLinha[4]);
-                $detalhe = explode(' in C', $ultimaCoisaNaLinha[1]);
+                $detalhe = $ultimaCoisaNaLinha[0]. " ". $ultimaCoisaNaLinha[1] . " " . $ultimaCoisaNaLinha[2];
                 $sendThings = array("titulo"=> $titulo, 
                                     "nivel" => $level, 
                                     "ip" => $ip[1], 
                                     "data_hora"=> $dataHora, 
-                                    "origem" => $origem[0],
-                                    "detalhe" => $detalhe[0],
-                                    "ambiente" => "Dev");
+                                    "origem" => $origem,
+                                    "detalhe" => $detalhe,
+                                    "ambiente" => $ambiente);
+                }else{
+
+                    $coisasNaLinha = explode('V',$linha);//todos os itens da linha em array
+                    $ultimaCoisaNaLinha = explode(':',$coisasNaLinha[5]);//ultimo item da linha, com o titulo e descrição
+                    $titulo = $ultimaCoisaNaLinha[0];
+                    $level = $coisasNaLinha[1];
+                    $dataHora = $coisasNaLinha[0];
+                    if($ultimaCoisaNaLinha[3]==null){
+                        $origem = str_replace('\\\\','\\',explode('in C', $ultimaCoisaNaLinha[2]));
+                    }else{
+                        $origem = str_replace('\\\\','\\',explode('in C', $ultimaCoisaNaLinha[3]));
+                    }
+                    $ip = explode('client ',$coisasNaLinha[4]);
+                    $detalhe = explode(' in C', $ultimaCoisaNaLinha[1]);
+                    $sendThings = array("titulo"=> $titulo, 
+                                        "nivel" => $level, 
+                                        "ip" => $ip[1], 
+                                        "data_hora"=> $dataHora, 
+                                        "origem" => $origem[0],
+                                        "detalhe" => $detalhe[0],
+                                        "ambiente" => $ambiente);
+                }
+                
+                
                 $sendJson = json_encode($sendThings);
                 echo $sendJson;
                 $ch = curl_init();
@@ -52,7 +74,7 @@ while(true){
                 $test = curl_setopt($ch, CURLOPT_POSTFIELDS, $sendJson);
                 echo "\n";
                 //set the content type to application/json
-                $test1 = curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json','Authorization:eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1NzYwOTM2OTEsImlhdCI6MTU3NTczMzY5MX0.OI0RXZFUhVZ9-y-48VL4_1IoZ7E3cvGBO2M9XmX6Zno'));
+                $test1 = curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json',"Authorization:$token"));
                 //return response instead of outputting
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
@@ -61,15 +83,14 @@ while(true){
 
                 //close cURL resource
                 curl_close($ch);
-
+                
                 $numeroAntigo = $numeroAtual;
                
                 // echo "\n$numeroAtual: ". $linha . "\n";
             }
             $numeroAtual++;
         }
-        $newHashedFile = md5_file('C:\xampp\apache\logs\error.log');
+        $newHashedFile = md5_file($caminho);
         fclose($arquivo);
     }
 }
-
