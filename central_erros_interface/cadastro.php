a15edde1ada5e402cf13661886093fd64c7c3e91
Melchiades de Medeiros Neto <melchiades.medeiros.neto@gmail.com>
2019-12-11 00:56:41 -0300

    Novas interfaces

--- central_erros_interface/cadastro.php
+++ central_erros_interface/cadastro.php
@@ -3,53 +3,135 @@
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-    <title>Cadastro</title>
-    
-    <meta charset="utf-8">
-    <meta name="description" content="Projeto Final Squad 1.">
-    <meta name="author" content="Squad_1">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="A fully featured admin theme which can be used to build CRM, CMS, etc.">
+		<meta name="author" content="Coderthemes">
 
-    <link rel="stylesheet" href="./css/bootstrap.min.3.4.0.css"> 
-    <link rel="stylesheet" href="./css/Login-Form-Clean.css">
-    <link rel="stylesheet" href="./css/signup.css">  
+		<title>Cadastro Central</title>
 
-    <script type="text/javascript">
-      var token_session='<?php echo $token_session;?>';  
-      var email_usuario='<?php echo $email_usu;?>';
-    </script>
+		<link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+		<link href="assets/css/core.css" rel="stylesheet" type="text/css"/>
+		<link href="assets/css/components.css" rel="stylesheet" type="text/css"/>
+		<link href="assets/css/icons.css" rel="stylesheet" type="text/css"/>
+		<link href="assets/css/pages.css" rel="stylesheet" type="text/css"/>
+		<link href="assets/css/responsive.css" rel="stylesheet" type="text/css"/>
 
-  </head>
-  <body class="text-center, bg">
-      <form action="" class="form-signin" method="post" id="forme" name="form">
-        <img class="mb-4" src="img/error-icon.png" alt="" width="72" height="72">
-        <h1 class="h3 mb-3 font-weight-normal" style="text-align: center">Cadastro do sistema</h1>
-        <label for="email" class="sr-only">Email address</label>
-        <input type="email" name="email" id="email" class="form-control" placeholder="E-mail"><br>
-        <label for="senha" class="sr-only">Password</label>
-        <input type="password" name="senha" id="senha" class="form-control" placeholder="Password">
-        <button class="btn btn-primary btn-block" type="submit">Enviar</button>
-        <button class="btn btn-primary btn-block" type="button" id="voltar">Voltar</button>
-        <a href="login.php" class="text-center">Já possuo Cadastro</a>
-        <p class="mt-5 mb-3 text-muted">Squad-1-vhsys © 2019</p>
-        <div class="response">
+		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+
+		<script src="assets/js/modernizr.min.js"></script>
+
+		<script type="text/javascript">
+		var token_session='<?php echo $token_session;?>';  
+		var email_usuario='<?php echo $email_usu;?>';
+		</script>
+
+	</head>
+	<body>
+
+		<div class="account-pages"></div>
+		<div class="clearfix"></div>
+		<div class="wrapper-page">
+			<div class=" card-box">
+				<div class="panel-heading">
+					<h3 class="text-center"> Cadastro em <strong class="text-custom">CentError</strong> </h3>
+				</div>
+
+				<div class="panel-body">
+					<form class="form-horizontal m-t-10" action="" method="post" id="forme" name="form">
+
+						<div class="form-group ">
+							<div class="col-xs-12">
+								<input class="form-control" type="email" name="email" id="email" required="" placeholder="E-mail">
+							</div>
+						</div>
+
+						<div class="form-group">
+							<div class="col-xs-12">
+								<input class="form-control" type="password" name="senha" id="senha" required="" placeholder="Senha">
+							</div>
+						</div>
+
+						<div class="form-group">
+							<div class="col-xs-12">
+								<div class="checkbox checkbox-primary">
+									<input id="checkbox-signup" type="checkbox" checked="checked">
+									<label for="checkbox-signup">Eu aceito os<a href="#"> Termos e Condições</a></label>
+								</div>
+							</div>
+						</div>
+
+						<div class="form-group text-center m-t-20">
+							<div class="col-xs-12">
+								<button class="btn btn-inverse btn-block text-uppercase waves-effect waves-light" type="submit">
+									Registrar
+								</button>
+							</div>
+						</div>
+
+						<div class="form-group text-center m-t-10">
+							<div class="col-xs-12">
+								<button class="btn btn-primary text-uppercase waves-effect waves-light" type="button" id="voltar">Voltar</button>
+							</div>
+                		</div>
+
+					</form>
+	
+				<div class="form-group text-center m-t-10 m-b-0">
+					<div class="col-sm-12">
+						<p><i class="fa fa-user m-r-5">
+						</i>Já possui Cadastro?<a href="login.php" class="text-primary m-l-5"><b>Entrar</b></a>
+						</p>
+					</div>
+				</div>
+
+			</div>
+		</div>
+
+		<footer class="footer text-center">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12">
+					Squad-1-vhsys © 2019.
+					</div>
+				</div>
+			</div>
+		</footer> 
+
+		<div class="text-center m-t-10 m-b-0">
           <!-- falhas em geral como as lanÃ§adas pelo serviÃ§o -->
-          <span class='msg-erro msg-falha'></span>
+          <span class='text-warning msg-erro msg-falha'></span>
           <!-- exemplo: completou algum processamento mas com alguma validaÃ§Ã£o nao obrigatoria -->
-          <span class='msg-alerta msg-warning'></span>
+          <span class='text-warning msg-alerta msg-warning'></span>
           <!-- exemplos: cadastrado, alterado, excluido com sucesso -->
-          <span class='msg-exito msg-sucesso'></span>
+          <span class='text-success msg-exito msg-sucesso'></span>
         </div>
-      </form>
-    </div>
 
-    <script src="./script/terceiros/jquery.min.3.4.1.js"></script>
-    <script src="./script/terceiros/popper.min.1.14.7.js"></script>
-    <script src="./script/terceiros/bootstrap.min.4.3.1.js"></script>
-    <script src="./script/comum/requisicaoAjax.js"></script>
-   
-    <script src="./script/comum/comum.js"></script>
-    <script src="./script/cadastro.js"></script>
-  </body>
+		<script>
+			var resizefunc = [];
+		</script>
+
+		<!-- jQuery  -->
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+        <script src="assets/js/detect.js"></script>
+        <script src="assets/js/fastclick.js"></script>
+        <script src="assets/js/jquery.slimscroll.js"></script>
+        <script src="assets/js/jquery.blockUI.js"></script>
+        <script src="assets/js/waves.js"></script>
+        <script src="assets/js/wow.min.js"></script>
+        <script src="assets/js/jquery.nicescroll.js"></script>
+        <script src="assets/js/jquery.scrollTo.min.js"></script>
+
+
+        <script src="assets/js/jquery.core.js"></script>
+        <script src="assets/js/jquery.app.js"></script>
+
+        <script src="./script/comum/requisicaoAjax.js"></script>
+        <script src="./script/comum/comum.js"></script>
+        <script src="./script/cadastro.js"></script>
+
+	</body>
 </html>
--- central_erros_interface/login.php
+++ central_erros_interface/login.php
@@ -1,55 +1,133 @@
 <?php
-  include 'config.php';
+    include 'config.php';
 ?>
 <!DOCTYPE html>
-<html lang="pt-br">
-<head>
-<title>Login</title>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="A fully featured admin theme which can be used to build CRM, CMS, etc.">
+        <meta name="author" content="Coderthemes">
 
-  <meta charset="utf-8">
-  <meta name="description" content="Projeto Final Squad 1.">
-  <meta name="author" content="Squad_1">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>Login Central</title>
 
-  <link rel="stylesheet" href="./css/bootstrap.min.3.4.0.css"> 
-  <link rel="stylesheet" href="./css/Login-Form-Clean.css">
-  <link rel="stylesheet" href="./css/signin.css">   
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/core.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/components.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/icons.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/pages.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/responsive.css" rel="stylesheet" type="text/css" />
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
 
-  <script type="text/javascript">
-    var token_session='<?php echo $token_session;?>';
-    var email_usuario='<?php echo $email_usu;?>';
-  </script>
+        <script src="assets/js/modernizr.min.js"></script>
+        <script type="text/javascript">
+            var token_session='<?php echo $token_session;?>';
+            var email_usuario='<?php echo $email_usu;?>';
+        </script>
+        
+    </head>
+    <body>
 
-</head>
-<body class="text-center, bg">
+        <div class="account-pages"></div>
+        <div class="clearfix"></div>
+        <div class="wrapper-page">
+        	<div class=" card-box">
+                <div class="panel-heading"> 
+                    <h3 class="text-center"> Entrar em <strong class="text-custom">CentError</strong> </h3>
+                </div> 
 
-  <form action="" class="form-signin" method="post" id="formLogin" name="form">
-    <img class="mb-4" src="img/error-icon.png" alt="" width="72" height="72">
-    <h1 class="h3 mb-3 font-weight-normal" style="text-align: center">Login do sistema</h1>
-    <label for="email" class="sr-only">Email address</label>
-    <input type="email" name="email" id="email" class="form-control"  placeholder="E-mail"><br>
-    <label for="senha" class="sr-only">Password</label>
-    <input type="password" name="senha" id="senha" class="form-control"  placeholder="Password">
-    <button class="btn btn-primary btn-block" type="submit">Entrar</button>
-        <button class="btn btn-primary btn-block" type="button" id="voltar">Voltar</button>
-    <a href="#" id="linkEsqueceuSenha" class="text-center">Esqueci minha Senha</a>
-    <p class="mt-5 mb-3 text-muted">Squad-1-vhsys © 2019</p>
-    <div class="response">
-      <!-- falhas em geral como as lanÃ§adas pelo serviÃ§o -->
-      <span class='msg-erro msg-falha'></span>
-      <!-- exemplo: completou algum processamento mas com alguma validaÃ§Ã£o nao obrigatoria -->
-      <span class='msg-alerta msg-warning'></span>
-      <!-- exemplos: cadastrado, alterado, excluido com sucesso -->
-      <span class='msg-exito msg-sucesso'></span>
-    </div>
-  </form>
-    
-    <script src="./script/terceiros/jquery.min.3.4.1.js"></script>
-    <script src="./script/terceiros/popper.min.1.14.7.js"></script>
-    <script src="./script/terceiros/bootstrap.min.4.3.1.js"></script>
-    <script src="./script/comum/requisicaoAjax.js"></script>
-    <script src="./script/comum/comum.js"></script>
-    <script src="./script/login.js"></script>
-    
-  </body>
+                <div class="panel-body">
+                    <form class="form-horizontal m-t-10" action="" method="post" id="formLogin" name="form">
+                        
+                        <div class="form-group">
+                            <div class="col-xs-12">
+                                <input class="form-control" name="email" id="email" type="text" required="" placeholder="E-mail">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-xs-12">
+                                <input class="form-control" name="senha" id="senha" type="password" required="" placeholder="Senha">
+                            </div>
+                        </div>
+
+                        <div class="form-group ">
+                            <div class="col-xs-12">
+                                <div class="checkbox checkbox-primary">
+                                    <input id="checkbox-signup" type="checkbox">
+                                    <label for="checkbox-signup">
+                                        Lembrar-me
+                                    </label>
+                                </div>
+                                
+                            </div>
+                        </div>
+                        
+                        <div class="form-group text-center m-t-10">
+                            <div class="col-xs-12">
+                                <button class="btn btn-inverse btn-block text-uppercase waves-effect waves-light" type="submit">Entrar</button>
+                            </div>
+                        </div>
+
+                        <div class="form-group text-center m-t-10">
+                            <div class="col-xs-12">
+                                <button class="btn btn-primary text-uppercase waves-effect waves-light" type="button" id="voltar">Voltar</button>
+                            </div>
+                        </div>
+
+                        <div class="form-group text-center m-t-10 m-b-0">
+                            <div class="col-sm-12">
+                                <a href="#" id="linkEsqueceuSenha" class="text-dark"><i class="fa fa-lock m-r-5"></i> Não lembra sua senha?</a>
+                            </div>
+                        </div>
+                    </form>
+                
+                </div>   
+        </div>
+        
+        <footer class="footer text-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12">
+                    Squad-1-vhsys © 2019.
+                    </div>
+                </div>
+            </div>
+        </footer>  
+        
+        <div class="text-center m-t-10 m-b-0">
+          <!-- falhas em geral como as lanÃ§adas pelo serviÃ§o -->
+          <span class='text-warning msg-erro msg-falha'></span>
+          <!-- exemplo: completou algum processamento mas com alguma validaÃ§Ã£o nao obrigatoria -->
+          <span class='text-warning msg-alerta msg-warning'></span>
+          <!-- exemplos: cadastrado, alterado, excluido com sucesso -->
+          <span class='text-success msg-exito msg-sucesso'></span>
+        </div>
+        
+    	<script>
+            var resizefunc = [];
+        </script>
+
+        <!-- jQuery  -->
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+        <script src="assets/js/detect.js"></script>
+        <script src="assets/js/fastclick.js"></script>
+        <script src="assets/js/jquery.slimscroll.js"></script>
+        <script src="assets/js/jquery.blockUI.js"></script>
+        <script src="assets/js/waves.js"></script>
+        <script src="assets/js/wow.min.js"></script>
+        <script src="assets/js/jquery.nicescroll.js"></script>
+        <script src="assets/js/jquery.scrollTo.min.js"></script>
+
+
+        <script src="assets/js/jquery.core.js"></script>
+        <script src="assets/js/jquery.app.js"></script>
+
+        <script src="./script/comum/requisicaoAjax.js"></script>
+        <script src="./script/comum/comum.js"></script>
+        <script src="./script/login.js"></script>
+	
+	</body>
 </html>
